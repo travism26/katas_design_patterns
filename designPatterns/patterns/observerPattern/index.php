eb7cbf52130b4edd3b4fd68f5ad128cd07bad375
travism26 <travismartin266@gmail.com>
2015-06-07 13:29:08 -0300

    clean up

--- designPatterns/patterns/observerPattern/index.php
+++ designPatterns/patterns/observerPattern/index.php
@@ -1,2 +1,41 @@
-<?php
-namespace pattern\observerPattern;
+<?php namespace pattern\observerPattern;
+require '../../index.php';
+
+class EmailNotifier implements Observer {
+
+	public function handle()
+	{
+		var_dump('send some emails out');
+	}
+}
+
+class  fileLogger implements Observer {
+
+	public function handle()
+	{
+		var_dump('log some file data');
+	}
+}
+
+class  adminNotify implements Observer {
+
+	public function handle()
+	{
+		var_dump('notify admin that new users has login.');
+	}
+}
+
+class  someOtherClass implements Observer {
+
+	public function handle()
+	{
+		var_dump('Do something....');
+	}
+}
+
+//lets just say this is our factory class below :)
+$login = new Login;
+$login->attach([new EmailNotifier, new fileLogger]);
+
+$login->attach(new someOtherClass())->attach(new adminNotify());
+$login->fire();
